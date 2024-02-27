e9f31673a94edca8c687485c7d33945baa166fe9
sqz0914 <sqz0914@gmail.com>
2024-02-27 10:56:16 -0800

    Update getURL.ts

--- app/upload/getURL.ts
+++ app/upload/getURL.ts
@@ -1,6 +1,6 @@
 'User Server'
 
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3"
+import { S3Client, PutObjectCommand, S3ClientConfig } from "@aws-sdk/client-s3"
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 type SignedURLResponse = Promise<
@@ -8,13 +8,15 @@ type SignedURLResponse = Promise<
   | { failure: string; success?: undefined }
 >
 
-const s3Client = new S3Client({
+const s3Config: S3ClientConfig = {
   region: process.env.NEXT_PUBLIC_AWS_BUCKET_REGION,
   credentials: {
     accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY,
     secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
   },
-})
+}
+
+const s3Client = new S3Client(s3Config);
 
 export async function getSignedURL(fileName: string): SignedURLResponse {
     const putObjectCommand = new PutObjectCommand({
